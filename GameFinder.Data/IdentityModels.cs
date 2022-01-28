14642099172b2978ebb524745afef6ccf6c87811
jims-jilmore <92383003+jims-jilmore@users.noreply.github.com>
2022-01-28 15:38:10 -0500

    Web.Config tweak

--- GameFinder.Data/IdentityModels.cs
+++ GameFinder.Data/IdentityModels.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity.ModelConfiguration;
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -29,6 +31,19 @@ namespace GameFinder.Data
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        //DB SETS HERE
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
         }
     }
 
